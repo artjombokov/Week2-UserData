8a8f457d1e55d8f5e70ba52c8658bbd989c857da
artjombokov <abokov@tktk.ee~>
2022-02-04 15:44:03 +0200

    Control Flow

---
+++ UserAge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //programm küsib kasutaja vanust;
+            //programm arvutab kasutaja sünniaastat;
+            //programm kuvab sünniaastat konsoolis;
+
+            Console.WriteLine("Palun, sisesta oma vanus:");
+            int ageOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int yearOfBirth = 2021 - ageOfBirth;
+            Console.WriteLine($"Oled sündinud aastal {yearOfBirth}.");
+
+
+        }
+    }
+}
